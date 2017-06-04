d9e0a0a4eb6cdb070be3e42b7e1d9a1194139479
BrendaCosta <brendacosta23@gmail.com>
2017-06-04 12:47:39 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+
+$('.flipcard').on('click', function() {
+  $(this).toggleClass('is-flipped');
+});
